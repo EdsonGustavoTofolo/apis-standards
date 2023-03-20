584e141e5ec704abf8709a3275a271fca5357035
Edson Tofolo <edsontofolo@gmail.com>
2023-03-20 17:10:21 -0300

    geracao do access token via swag e correcao da busca do usuario pelo e-mail para usar First ao inves de Find

--- internal/infra/webserver/handlers/user_handlers.go
+++ internal/infra/webserver/handlers/user_handlers.go
@@ -10,6 +10,10 @@ import (
 	"time"
 )
 
+type Error struct {
+	Message string `json:"message"`
+}
+
 type UserHandler struct {
 	UserDB database.UserRepository
 }
@@ -20,11 +24,23 @@ func NewUserHandler(db database.UserRepository) *UserHandler {
 	}
 }
 
+// Create user godoc
+// @Summary Create user
+// @Description Create user
+// @Tags users
+// @Accept json
+// @Produce json
+// @Param request body dto.CreateUserInput true "user request"
+// @Success 201
+// @Failure 500 {object} Error
+// @Router /users [post]
 func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	var user dto.CreateUserInput
 
 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 
@@ -32,17 +48,33 @@ func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 
 	if err := h.UserDB.Create(u); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 
+	w.Header().Add("Location", r.URL.Path+"/"+u.ID.String())
 	w.WriteHeader(http.StatusCreated)
 }
 
+// GetJWT godoc
+// @Summary Get a user JWT
+// @Description Get a user JWT
+// @Tags users
+// @Accept json
+// @Produce json
+// @Param request body dto.GetJwtInput true "user credentials"
+// @Success 200 {object} dto.GetJwtOutput
+// @Failure 404
+// @Failure 500 {object} Error
+// @Router /users/token [post]
 func (h *UserHandler) GetJwt(w http.ResponseWriter, r *http.Request) {
 	jwt := r.Context().Value("jwt").(*jwtauth.JWTAuth)
 	jwtExpiresIn := r.Context().Value("jwtExpiresIn").(int)
@@ -51,18 +83,24 @@ func (h *UserHandler) GetJwt(w http.ResponseWriter, r *http.Request) {
 
 	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 
 	user, err := h.UserDB.FindByEmail(body.Email)
 
 	if err != nil {
-		w.WriteHeader(http.StatusUnauthorized)
+		w.WriteHeader(http.StatusNotFound)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 
 	if !user.ValidatePassword(body.Password) {
 		w.WriteHeader(http.StatusUnauthorized)
+		error := Error{Message: "User or password is not valid"}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 
@@ -71,13 +109,10 @@ func (h *UserHandler) GetJwt(w http.ResponseWriter, r *http.Request) {
 		"exp": time.Now().Add(time.Second * time.Duration(jwtExpiresIn)).Unix(),
 	})
 
-	accesToken := struct {
-		AccesToken string `json:"access_token"`
-	}{
-		AccesToken: token,
-	}
+	accesToken := dto.GetJwtOutput{AccessToken: token}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
+
 	json.NewEncoder(w).Encode(accesToken)
 }
